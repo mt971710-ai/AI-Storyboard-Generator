dc8a06251d3e3031c6f0daf9f43cbbad60d758e0
mt971710-ai <mt971710@gmail.com>
2025-11-03 21:31:48 +0200

    Update ai_studio_code.sh

--- ai_studio_code.sh
+++ ai_studio_code.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "Initial commit"
 git branch -M main
-git remote add origin https://github.com/<YOUR_USERNAME>/<YOUR_REPOSITORY_NAME>.git
+git remote add origin https://github.com/<mt971710-ai>/<AI-Storyboard-Generator>.git
 git push -u origin main
